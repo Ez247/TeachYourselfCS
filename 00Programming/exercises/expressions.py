0bb732c49bf24502bcc0289c4be8e77d74d1d920
Kuluny <lokilek2446@gmail.com>
2022-06-22 22:55:38 -0400

    Primitive and compound expressions

---
+++ 00Programming/exercises/expressions.py
@@ -0,0 +1,23 @@
+"""Expressions:
+        0. Expressions representing numbers may be combined with mathematical 
+           operators to form compound expressions.
+"""
+
+# Primitve expresion
+n = 42
+print(n)
+
+#Compound expression
+s = 1/2 + 1/4 + 1/8 + 1/16 + 1/32 + 1/64 + 1/128
+print(s)
+
+
+"""Call Expression:
+        0. The most important kind of compound expression.
+        1. This applies a function to some arguments. 
+"""
+
+#Examples
+max(7.5, 9.5)  # Returns 9.5
+pow(100, 2)  # Returns 10000
+pow(2, 100)  # Returns 1267650600228229401496703205376
